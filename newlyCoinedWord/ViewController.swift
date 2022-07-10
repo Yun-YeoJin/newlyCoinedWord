ad0bf3e998d5f34ef1a79eb3b1a2000263e76b49
YunYeoJin <duwls0349@naver.com>
2022-07-10 14:54:20 +0900

    newlyCoinedWord project step1, step2, step3 result

--- newlyCoinedWord/ViewController.swift
+++ newlyCoinedWord/ViewController.swift
@@ -1,19 +1,61 @@
-//
-//  ViewController.swift
-//  newlyCoinedWord
-//
-//  Created by 윤여진 on 2022/07/10.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var SearchBar: UITextField!
+    @IBOutlet weak var SearchButton: UIButton!
+    
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    @IBOutlet weak var thirdButton: UIButton!
+    @IBOutlet weak var fourthButton: UIButton!
+    
+    @IBOutlet weak var resultSearch: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        buttonDesign(firstButton, "만반잘부")
+        buttonDesign(secondButton, "꾸안꾸")
+        buttonDesign(thirdButton, "낄낄빠빠")
+        buttonDesign(fourthButton, "솔까말")
+        
+        labelDesign()
+        
     }
+    
+    //각 버튼 Design 함수 설정
+    func buttonDesign(_ buttonNumber: UIButton, _ buttonName: String) {
+        buttonNumber.setTitle(buttonName, for: .normal)
+        buttonNumber.setTitleColor(.black, for: .normal)
+        buttonNumber.titleLabel?.font = .systemFont(ofSize: 15)
+        buttonNumber.layer.cornerRadius = 8
+        buttonNumber.layer.borderWidth = 2
+        buttonNumber.layer.borderColor = UIColor.darkGray.cgColor
+        buttonNumber.clipsToBounds = true
+        
 
 
 }
-
+    
+    func labelDesign() {
+        resultSearch.text = "신조어 검색 결과"
+        resultSearch.font = UIFont.boldSystemFont(ofSize: 20)
+        resultSearch.textColor = UIColor.blue
+        resultSearch.textAlignment = .center
+    }
+    //올라와있는 키보드 내리기
+    @IBAction func keyBoardDown(_ sender: Any) {
+        view.endEditing(true)
+    }
+    @IBAction func searchButtonClicked(_ sender: UIButton) {
+        resultSearch.text = SearchBar.text
+    }
+    
+    @IBAction func keyBoardDownForReturn(_ sender: UITextField) {
+        resultSearch.text = SearchBar.text
+    }
+    
+    
+}
